854c4d8cef88707e3fde0adf6c4d30ad6e061027
Pramod <pgoudar347@gmail.com>
2024-11-13 16:42:06 +0530

    Tailwind CSS implemented

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -17,6 +17,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
